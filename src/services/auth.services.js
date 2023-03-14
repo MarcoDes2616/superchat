e431b16a0294491a969337fa798e793e5dc4d384
Marco Cardenas <112307097+MarcoDes2616@users.noreply.github.com>
2023-03-14 17:54:42 -0500

    Update auth.services.js

--- src/services/auth.services.js
+++ src/services/auth.services.js
@@ -3,9 +3,9 @@ const jwt = require("jsonwebtoken");
 class AuthServices {
   static genToken(payload) {
     try {
-      const token = jwt.sign(payload, "iannacus", {
+      const token = jwt.sign(payload, "superchat", {
         algorithm: "HS512",
-        expiresIn: "5m",
+        expiresIn: "60m",
       });
       return token;
     } catch (error) {
